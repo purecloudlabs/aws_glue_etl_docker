bb5406c9348b073c616cd059501aca492516e94b
Kevin Glinski <kevin.glinski@inin.com>
2018-11-06 12:06:48 -0500

    removing boto3 as explicit dep

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version = '0.4.0',
     packages = ['aws_glue_etl_docker'],
     license='mit',
-    install_requires = ['boto3'],
+
     entry_points = {
         'console_scripts': [
             'aws_glue_etl_docker = aws_glue_etl_docker.__main__:main'
