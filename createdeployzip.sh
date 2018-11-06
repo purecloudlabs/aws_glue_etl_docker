3adb4c39f5e31959f51fe9708dd154d87f59de46
Kevin Glinski <kevin.glinski@inin.com>
2018-11-06 13:21:49 -0500

    version bump

--- createdeployzip.sh
+++ createdeployzip.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 rm -rf aws_glue_etl_docker_deploy
 mkdir aws_glue_etl_docker_deploy
 cd aws_glue_etl_docker_deploy
-echo "git+https://github.com/purecloudlabs/aws_glue_etl_docker" > requirements.txt
 mkdir deps
 virtualenv -p python2.7 .
-bin/pip2.7 install -r requirements.txt --install-option --install-lib="$(PWD)/deps"
+pip install -t deps git+https://github.com/purecloudlabs/aws_glue_etl_docker
 cd deps && zip -r ../aws_glue_etl_docker_deploy.zip . && cd ..
 aws s3 cp ./aws_glue_etl_docker_deploy.zip $1
-  
+ 
